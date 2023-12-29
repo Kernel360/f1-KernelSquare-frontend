56d457e11f7fe48173cf5d3865da30d6f1d59bdc
JeongwooHam <hamjw0122@gmail.com>
2023-12-29 15:39:18 +0900

    feat: code-syntax-highlight 기능 구현

--- src/page/qna-detail/components/Markdown/MdEditor.tsx
+++ src/page/qna-detail/components/Markdown/MdEditor.tsx
@@ -1,13 +1,19 @@
 "use client"
 
+import "prismjs/themes/prism.css"
+import Prism from "prismjs"
+
 import "@toast-ui/editor/dist/toastui-editor.css"
 import "tui-color-picker/dist/tui-color-picker.css"
-import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css"
-import colorSyntax from "@toast-ui/editor-plugin-color-syntax"
 import { Editor } from "@toast-ui/react-editor"
-import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight"
+// 텍스트 색상 변경 지원
+import colorSyntax from "@toast-ui/editor-plugin-color-syntax"
+// 코드 하이라이트 지원
+import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css"
+// 전체 언어 지원
+import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js"
 import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css"
-import { RefObject } from "react"
+import type { RefObject } from "react"
 
 interface EditorProps {
   previous?: string
@@ -28,7 +34,7 @@ const MdEditor: React.FC<EditorProps> = ({ previous, editorRef }) => {
           height="300px"
           // toolbarItems={toolbarItems}
           useCommandShortcut={true}
-          plugins={[colorSyntax, codeSyntaxHighlight]}
+          plugins={[colorSyntax, [codeSyntaxHighlight, { highlighter: Prism }]]}
         />
       )}
     </div>
