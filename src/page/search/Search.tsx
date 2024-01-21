fc159fb56d6d9b0c8856fb05859ec46349eb245d
JeongwooHam <hamjw0122@gmail.com>
2024-01-22 02:43:40 +0900

    design: 검색 페이지 UI 구현

---
+++ src/page/search/Search.tsx
@@ -0,0 +1,13 @@
+import WithResult from "./WithResult"
+import WithoutResult from "./WithoutResult"
+
+const Search = () => {
+  return (
+    <div>
+      <WithResult />
+      {/* <WithoutResult /> */}
+    </div>
+  )
+}
+
+export default Search
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss"
-import { fontFamily } from "tailwindcss/defaultTheme"
 
 const config = {
   darkMode: ["class"],
@@ -98,9 +97,6 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-      },
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
   },
