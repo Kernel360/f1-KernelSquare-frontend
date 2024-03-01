d53f1b373bb56cac5d07e673e7e795f4d069c4b5
Ryan-TheLion <dae7249@nate.com>
2024-03-01 22:59:43 +0900

    fix: 카카오 맵, 시간
    - 카카오 맵 로드, 에러 렌더링
    - 카카오 맵 에러 발생시 센트리 에러 전송
    - 기본 스크롤 이벤트(줌) 취소, 버튼으로 직접 줌인
    줌아웃 할 수 있도록 구현
    - 카카오 맵 컴포넌트 dynamic 클라이언트 렌더링
    - 서버시간을 한국시간으로 변환(dynamic 클라이언트 렌더링)시 로딩 렌더링

--- src/page/coding-meetings/detail/kakao-map/Map.tsx
+++ src/page/coding-meetings/detail/kakao-map/Map.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk"
+import DetailMapControl from "@/page/coding-meetings/detail/kakao-map/Control"
 
 interface DetailMapProps {
   x: string
@@ -9,10 +10,18 @@ interface DetailMapProps {
 }
 
 function DetailMap({ x, y, placeName }: DetailMapProps) {
-  useKakaoLoader({
+  const [loading, error] = useKakaoLoader({
     appkey: process.env.NEXT_PUBLIC_KAKAO_MAP!,
     libraries: ["services"],
   })
+
+  const zoomLevel = {
+    max: 1,
+    min: 6,
+    initial: 3,
+  }
+
+  if (error) throw error
 
   return (
     <Map
@@ -20,13 +29,24 @@ function DetailMap({ x, y, placeName }: DetailMapProps) {
         lat: Number(x),
         lng: Number(y),
       }}
-      level={3}
+      zoomable={false}
+      level={zoomLevel.initial}
+      maxLevel={zoomLevel.max}
+      minLevel={zoomLevel.min}
       style={{
         width: "100%",
         height: "100%",
       }}
     >
       <MapMarker position={{ lat: Number(x), lng: Number(y) }} />
+      <DetailMapControl
+        maxZoomLevel={zoomLevel.max}
+        minZoomLevel={zoomLevel.min}
+        location={{
+          x: Number(x),
+          y: Number(y),
+        }}
+      />
     </Map>
   )
 }
