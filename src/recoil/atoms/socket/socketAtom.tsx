8933c236632885271fe2acd91e3563eb0613dcae
Ryan-TheLion <dae7249@nate.com>
2024-03-02 19:48:41 +0900

    chore: 채팅 메시지 타입 수정
    변경된 타입으로 수정

--- src/recoil/atoms/socket/socketAtom.tsx
+++ src/recoil/atoms/socket/socketAtom.tsx
@@ -1,11 +1,20 @@
 import { atomFamily } from "recoil"
 
+export interface ChatRoomMember {
+  member_id: number
+  nickname: string
+  member_image_url: string | null
+}
+
 export interface MessagePayload {
   type: "ENTER" | "TALK" | "LEAVE" | "CODE" | "EXPIRE"
   room_key: string
+  sender_id: number
   sender: string
+  sender_image_url: string | null
   message: string
   send_time?: string
+  member_list: Array<ChatRoomMember>
 }
 
 interface Room {
