a0556eab5a49dfb064edb9e7984c69b93e147e8d
Ryan-TheLion <dae7249@nate.com>
2024-03-03 12:15:05 +0900

    feat: mock stomp 소켓 서버 중간 저장
    - 이후 이어서 구현 시도

--- src/socket/server/index.js
+++ src/socket/server/index.js
@@ -1,36 +1,30 @@
 const http = require("http")
 const sockjs = require("sockjs")
 const StompServer = require("stomp-broker-js")
-// const StompJs = require('@stomp/stompjs');
 
 const PORT = 8000
 
-// Create a SockJS server
 const sockjsServer = sockjs.createServer()
 sockjsServer.on("connection", function (conn) {
   console.log("Client connected")
 
-  // STOMP 프로토콜 처리
-  const server = new StompServer({
-    protocol: new StompServer.NodeStompServerProtocol(conn),
-  })
+  // const server = new StompServer({
+  //   protocol: new StompServer.NodeStompServerProtocol(conn),
+  // })
 
-  server.subscribe("/topic/chat", function (body, headers) {
-    console.log("Received message:", body)
-    // 가공된 메시지를 클라이언트로 전송
-    conn.write(body)
-  })
+  // server.subscribe("/topic/chat", function (body, headers) {
+  //   console.log("Received message:", body)
+  //   conn.write(body)
+  // })
 
   conn.on("close", function () {
     console.log("Client disconnected")
   })
 })
 
-// Create a Node.js HTTP server
 const server = http.createServer()
-sockjsServer.installHandlers(server, { prefix: "/ws" })
+sockjsServer.installHandlers(server, { prefix: "/kernel-square" })
 
-// Start listening
 server.listen(PORT, () => {
   console.log(`WebSocket server listening on port ${PORT}`)
 })
