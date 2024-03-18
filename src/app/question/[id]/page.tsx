cffe9c431f9450e59eb27e5e21a08c409ca51030
JeongwooHam <hamjw0122@gmail.com>
2024-03-19 01:42:53 +0900

    feat: 개발자 Q&A 상세 페이지 metadata 생성

--- src/app/question/[id]/page.tsx
+++ src/app/question/[id]/page.tsx
@@ -2,22 +2,51 @@ import { layoutMeta } from "@/constants/layoutMeta"
 import QnADetail from "@/page/qna-detail/QnADetail"
 import { Metadata } from "next"
 
-interface QuestionDetailPage {
+interface QuestionDetailPageProps {
   params: {
     id: string
   }
 }
 
-/*
-  [TODO]
-  generateMetadata로 각 질문글에 해당하는
-  동적 metadata 생성
-*/
+function isValidPageProps({ params }: QuestionDetailPageProps) {
+  const id = params.id
+
+  if (Number(id) < 0 || params.id.includes(".") || Number.isNaN(Number(id))) {
+    return false
+  }
+
+  return true
+}
+
+export async function generateMetadata({
+  params,
+}: QuestionDetailPageProps): Promise<Metadata> {
+  if (!isValidPageProps({ params })) {
+    return {
+      title: `존재하지 않는 페이지`,
+      description: `존재하지 않는 페이지에 대한 접근입니다.`,
+    }
+  }
+  try {
+    return {
+      title: `개발자 Q&A 상세 페이지`,
+      description: `개발자 Q&A 상세 페이지`,
+    }
+  } catch (error) {
+    return {
+      title: `잘못된 페이지 요청`,
+      description: `잘못된 페이지 요청입니다. 올바른 형식의 페이지를 요청해주세요.`,
+    }
+  }
+}
+
 export const metadata: Metadata = {
   title: `${layoutMeta["question"].title}`,
   description: `${layoutMeta["question"].description}`,
 }
 
-export default function QuestionDetailPage({ params }: QuestionDetailPage) {
+export default function QuestionDetailPage({
+  params,
+}: QuestionDetailPageProps) {
   return <QnADetail id={params.id} />
 }
