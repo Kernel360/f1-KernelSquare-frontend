013091137ce19daf3425261a3ee2f467e6d28083
Ryan-TheLion <dae7249@nate.com>
2024-04-01 22:52:47 +0900

    fix: 모각코 상세 url
    - 카카오톡 소셜 미리보기에서
    invalid url 로 적용되지 않는 형식으로
    수정 해 봄

--- src/page/coding-meetings/util/cm-token.ts
+++ src/page/coding-meetings/util/cm-token.ts
@@ -1,7 +1,7 @@
 export function serializeCmToken(cmToken: string) {
-  return cmToken.replace(/^cm_/g, "cm-")
+  return cmToken.replace(/^cm_/g, "cm")
 }
 
 export function deSerializeCmToken(serializedCmToken: string) {
-  return `${serializedCmToken.replace(/^cm\-/g, "cm_")}`
+  return `${serializedCmToken.replace(/^cm/g, "cm_")}`
 }
