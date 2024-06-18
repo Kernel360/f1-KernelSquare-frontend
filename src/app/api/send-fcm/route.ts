7b1379bf3755b6d8dc1778e02f5bcbbf0d0f3405
Ryan-TheLion <dae7249@nate.com>
2024-06-18 17:12:57 +0900

    fix: PEM 개행문자 처리 방식 수정

--- src/app/api/send-fcm/route.ts
+++ src/app/api/send-fcm/route.ts
@@ -32,7 +32,7 @@ export async function POST(request: NextRequest) {
     : admin.initializeApp({
         credential: admin.credential.cert({
           projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         }),
       })
